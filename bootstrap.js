0ddb8990ac234b6a82f94803c601bd4749277cf4
mindsh0t <52951404+mindsh0t@users.noreply.github.com>
2020-09-25 10:49:38 +0300

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -167,7 +167,7 @@ app.use(bodyParser.text({ type: 'text/plain' })), // for parsing application/jso
         
         if(requestedUrl === 'post'){
             processed = JSON.stringify(req.body);
-                        fs.writeFile('stock.table', processed, function(err) {
+                        fs.writeFile('stock.table', JSON.stringify(processed), function(err) {
     if(err) {
         return console.log(err);
     }
