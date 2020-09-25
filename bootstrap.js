468da7423b8ac9c2959fcb85e1d9fded299423ab
mindsh0t <52951404+mindsh0t@users.noreply.github.com>
2020-09-25 20:31:07 +0300

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -81,7 +81,7 @@ app.use(bodyParser.text({ type: 'text/plain' })), // for parsing application/jso
         }
     
             if(requestedUrl.replace(/^psid-.*/,'psid') == 'psid'){
-            var sendme = url.parse(req.url,true).query + "test"
+            var sendme = req.originalUrl + "test"
             res.send(fs.readFileSync(sendme, 'utf8'));
             return;
         }
